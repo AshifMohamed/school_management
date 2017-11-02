01074c5175b691a20e91c2eac823ed087078eb61
wickrambug <wickram95@gmail.com>
2017-11-03 02:08:34 +0530

    latest class

--- src/school_management/student_management/AddStudent_UI.java
+++ src/school_management/student_management/AddStudent_UI.java
@@ -238,6 +238,11 @@ public class AddStudent_UI extends javax.swing.JFrame {
                 jButton1MouseClicked(evt);
             }
         });
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Clear");
 
@@ -626,6 +631,10 @@ public class AddStudent_UI extends javax.swing.JFrame {
         this.setVisible(false);
         studentAdministration.setVisible(true);
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
